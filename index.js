15c99747e83c7503fca1cbce679711c07218d4a1
yashbellam97 <yashbellam97@gmail.com>
2020-10-17 12:46:16 -0400

    Add an array of links

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const linksArray = [
+    { "name": "Cloudflare", "url": "https://www.cloudflare.com/" },
+    { "name": "Google", "url": "https://www.google.com/" },
+    { "name": "Mozilla", "url": "https://www.mozilla.org/" }
+]
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
  * Respond with hello worker text
  * @param {Request} request
