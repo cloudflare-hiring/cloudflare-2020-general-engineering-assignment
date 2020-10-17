b606102cfa4f2ab8ba84af5f12682a65fe709925
yashbellam97 <yashbellam97@gmail.com>
2020-10-17 16:59:17 -0400

    Add profile details

--- index.js
+++ index.js
@@ -41,7 +41,25 @@ class LinkTransformer {
     }
 }
 
+class ProfileTransformer {
+    async element(element) {
+        if (element) {
+            if (element.tagName === "div") {
+                element.removeAttribute("style");
+            } else if (element.tagName === "img") {
+                element.setAttribute("src", "https://avatars0.githubusercontent.com/u/34393703?s=460&u=37af9117851ee4eaf8afdd7b642648a60edb4831&v=4");
+            } if (element.tagName === "h1") {
+                element.setInnerContent("Yaswanth Bellam");
+            }
+        }
+    }
+}
+
 const linkTransformer = new HTMLRewriter().on('div#links', new LinkTransformer(linksArray));
+
+const profileTransformer = new HTMLRewriter().on('div#profile', new ProfileTransformer(linksArray));
+const profileImageTransformer = new HTMLRewriter().on('img#avatar', new ProfileTransformer());
+const profileNameTransformer = new HTMLRewriter().on('h1#name', new ProfileTransformer());
 
 async function getStaticPage() {
     let staticPage = await fetch('https://static-links-page.signalnerve.workers.dev')
@@ -51,8 +69,11 @@ async function getStaticPage() {
             } else new Response('Something went wrong!', { status: 500 });
         })
 
-
     staticPage = linkTransformer.transform(staticPage);
+
+    staticPage = profileTransformer.transform(staticPage);
+    staticPage = profileImageTransformer.transform(staticPage);
+    staticPage = profileNameTransformer.transform(staticPage);
 
     return staticPage;
 }
